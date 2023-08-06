48e826f2ce9e185fc2ea5ac88a2693546bfa99eb
Syed Shayan <shayansyed712@gmail.com>
2023-08-06 12:48:10 +0500

    next commit

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import Form from "@/components/auth/form";
-
+import { getSession } from "next-auth/react";
 export default function SignUp () {
     const onSubmit = async (email, password) => {
         try {
@@ -20,3 +20,21 @@ export default function SignUp () {
     };
     return <Form signin={false} onFormSubmit={onSubmit} />
 };
+
+export const getServerSideProps = async ({ req }) => {
+    const session = await getSession({ req });
+    if (session) {
+      return {
+        redirect: {
+          destination: "./login",
+          parmanent: false,
+        },
+      };
+    }
+  
+    return {
+      props: {
+        session,
+      },
+    };
+  };
